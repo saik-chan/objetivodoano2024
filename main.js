348e2cda8d7b5aace89d9578da3dfc6262ed3400
saik-chan <107414257+saik-chan@users.noreply.github.com>
2024-03-27 16:25:10 -0300

    main.js

--- main.js
+++ main.js
@@ -15,9 +15,14 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-05T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos =[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+contadores[0].textContent = calculaTempo(tempos[0]); 
+
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -29,7 +34,22 @@ function calculaTempo(tempoObjetivo) {
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
-return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+if (tempoFinal > 0){
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Prazo Finalizado";
+}
 
 
 }
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+function comecaCronometro(){
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
+
+}
+comecaCronometro();
